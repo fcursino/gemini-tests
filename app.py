d467c822567a597e5fd98cae1fb8dfb4d22ee9ca
fcursino <fabriciocursino09@gmail.com>
2024-06-08 21:42:58 -0300

    implementing dotenv lib to use .env file

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', api_key=api_key)
 
 if __name__ == "__main__":
     app.run()
